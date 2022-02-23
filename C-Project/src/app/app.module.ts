7876b34a050b73424584fa937bf8de01372c0600
Gentiana-Hoxha <60153694+Gentiana-Hoxha@users.noreply.github.com>
2022-02-23 17:15:52 +0200

    Secondary Components

--- C-Project/src/app/app.module.ts
+++ C-Project/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation-panel/navigation.component';
 import { ContentAreaComponent } from './content-area/content-area.component';
+import { ContentAreaHeaderComponent } from './content-area-header/content-area-header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavigationComponent,
-    ContentAreaComponent
+    ContentAreaComponent,
+    ContentAreaHeaderComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
