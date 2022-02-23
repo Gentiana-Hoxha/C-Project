4c9231634988285a3b6a21e18a2bdd3ad7fdf083
Gentiana-Hoxha <60153694+Gentiana-Hoxha@users.noreply.github.com>
2022-02-23 17:10:09 +0200

    First Layout Base
    3 main components

--- C-Project/src/app/app.module.ts
+++ C-Project/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation-panel/navigation.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule
